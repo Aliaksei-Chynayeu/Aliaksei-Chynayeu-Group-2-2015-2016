2aea7dd86820c7e4ab27538c9a43ca4b5411be0d
aliaksei-hlazkou <a.hlazkou@gmail.com>
2016-02-23 19:04:56 +0300

    gradle v2

---
+++ task-manager-dep/task-manager-business-logic/src/test/java/com/epam/test/simul/BuisnessLogicTestSimulator.java
@@ -0,0 +1,19 @@
+package com.epam.test.simul;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Aliaksei_Hlazkou
+ */
+public class BuisnessLogicTestSimulator  extends Assert {
+
+		@Test
+		public void testBad() {
+		
+		}
+
+		@Test
+		public void testGood()  {
+		}
+
+	}
